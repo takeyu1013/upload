0e0984d37e2568625c8f9a82b6441a8b183109cd
takeyu1013 <yuto1013@gmail.com>
2022-06-25 16:03:54 +0900

    ✨

--- clerk/src/pages/index.tsx
+++ clerk/src/pages/index.tsx
@@ -1,11 +1,15 @@
-import { useAuth, useSignIn, useUser } from "@clerk/nextjs";
 import type { NextPage } from "next";
+
 import Image from "next/image";
+import { useState } from "react";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { useAuth, useSignIn, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
   const { signIn } = useSignIn();
   const { signOut } = useAuth();
   const { user, isSignedIn } = useUser();
+  const [file, setFile] = useState<File | undefined>(undefined);
 
   return (
     <div>
@@ -41,6 +45,47 @@ const Home: NextPage = () => {
           Login
         </button>
       )}
+      <div>
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={(event) => {
+            const { files } = event.target;
+            if (!files) {
+              return;
+            }
+            setFile(files[0]);
+          }}
+        />
+      </div>
+      <button
+        onClick={async () => {
+          if (!file) {
+            return;
+          }
+          const name = encodeURIComponent(file.name);
+          const type = encodeURIComponent(file.type);
+          const client = new S3Client({
+            region: "ap-northeast-1",
+            credentials: {
+              accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY as string,
+              secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY as string,
+            },
+          });
+
+          const output = await client.send(
+            new PutObjectCommand({
+              Bucket: process.env.NEXT_PUBLIC_BUCKET_NAME,
+              Key: name,
+              ContentType: type,
+              Body: file,
+            })
+          );
+          console.log(output);
+        }}
+      >
+        Upload
+      </button>
     </div>
   );
 };
---
+++ clerk/terraform/main.tf
@@ -0,0 +1,3 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+}
