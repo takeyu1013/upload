c1240e17c6fd71b6832acc9b44e9cb9a34e214c0
takeyu1013 <yuto1013@gmail.com>
2022-07-03 13:14:01 +0900

    🚧

--- gcp/src/pages/index.tsx
+++ gcp/src/pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return <main>foo</main>;
+  const [file, setFile] = useState<File | undefined>(undefined);
+
+  return (
+    <main>
+      <input
+        type="file"
+        accept="image/png, image/jpg"
+        onChange={(event) => {
+          const { files } = event.target;
+          if (!files) return;
+          setFile(files[0]);
+        }}
+      />
+      <button onClick={() => {}}>Upload</button>
+    </main>
+  );
 };
 
 export default Home;
